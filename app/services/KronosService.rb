8497c9876b4dec3d4b1e75dc48be9320b58db2c5
Josue Mente <josuemente@hotmail.com>
2024-07-20 17:46:06 -0600

    service to assign hours

--- app/services/KronosService.rb
+++ app/services/KronosService.rb
@@ -4,25 +4,31 @@ class KronosService
   def assign_services
     Service.all.each do |service|
       weeks_available.each do |week|
-        Catalog::Hour::DAYS.each_with_index do |day, day_index|
-          service_hours = service.service_hours.where(day: day_index)
-
-          service_hours.each do |service_hour|
-            available_hour = find_available_hour(week, day_index, service_hour.catalog_hours_id)
-
-            next if available_hour.blank?
-
-            available_hour.update(services_id: service_hour.services_id)
-
-            Rails.logger.info "*** #{service.name} Assigned to #{available_hour.user.name} on #{day} at #{service_hour.catalog_hour.start_at_time} - #{service_hour.catalog_hour.end_at_time}"
-          end
-        end
+        assign_service_to_week(service, week)
       end
     end
   end
 
+  private
+
+  def assign_service_to_week(service, week)
+    Catalog::Hour::DAYS.each_with_index do |_day, day_index|
+      service.service_hours.where(day: day_index).each do |service_hour|
+        assign_hour_to_day(service, week, day_index, service_hour)
+      end
+    end
+  end
+
+  def assign_hour_to_day(service, week, day_index, service_hour)
+    available_hour = find_available_hour(week, day_index, service_hour.catalog_hours_id)
+    return if available_hour.blank?
+
+    available_hour.update(services_id: service_hour.services_id)
+    log_assignment(service, available_hour, day_index, service_hour)
+  end
+
   def weeks_available
-    Users::Availability.where(available: true).where(services_id: nil).pluck(:week).uniq
+    Users::Availability.where(available: true, services_id: nil).pluck(:week).uniq
   end
 
   def find_available_hour(week, day_index, catalog_hours_id)
@@ -36,5 +42,9 @@ class KronosService
       users_id: :asc,
       updated_at: :asc
     ).first
+  end
+
+  def log_assignment(service, available_hour, day_index, service_hour)
+    Rails.logger.info "*** #{service.name} Assigned to #{available_hour.user.name} on #{Catalog::Hour::DAYS[day_index]} at #{service_hour.catalog_hour.start_at_time} - #{service_hour.catalog_hour.end_at_time}"
   end
 end
